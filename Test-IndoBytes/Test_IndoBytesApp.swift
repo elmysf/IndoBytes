5175aa29dc17508b871a1490e6c13a9981c7105b
elmysf <elmysufiandy09@gmail.com>
2024-08-25 09:30:42 +0700

    Initial Commit

---
+++ Test-IndoBytes/Test_IndoBytesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_IndoBytesApp.swift
+//  Test-IndoBytes
+//
+//  Created by Sufiandy Elmy on 25/08/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_IndoBytesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
